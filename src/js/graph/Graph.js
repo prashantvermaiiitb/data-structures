da8ec319c4382892fee80a73ec6a31dba326aae1
Prashant Verma <prashant.verma@timesinternet.in>
2020-11-28 15:26:40 +0530

    included handlign for the topological sorting.

--- src/js/graph/Graph.js
+++ src/js/graph/Graph.js
@@ -1,6 +1,7 @@
 var depthFirstSearch = require("./DepthFirstSearch");
 var breadthFirstSearch = require("./BreadthFirstSearch");
 var minimumSpanningTree = require("./MinimumSpanningTree");
+var topologicalSort = require("./TopologicalSorting");
 /**
  * Sample class for the Graph implementation.
  */
@@ -75,10 +76,11 @@ class Graph {
    * Adding the edge in the graph
    * @param {*} from
    * @param {*} to
+   * @param {boolean} isDag : represents whether the graph is Directed Acyclic graph
    */
-  addEdge(from, to) {
+  addEdge(from, to, isDAG = false) {
     this.adjacentMatrix[from][to] = 1;
-    this.adjacentMatrix[to][from] = 1;
+    !isDAG && (this.adjacentMatrix[to][from] = 1);
   }
   /**
    * display the vertex
@@ -135,6 +137,141 @@ class Graph {
   getVertexList() {
     return this.vertexList;
   }
+  /**
+   * returning the number of current vertices
+   */
+  getCurrentVertices() {
+    return this.currentVertices;
+  }
+
+  /**
+   * searching the graph to get the node with no starting edge
+   * this node will have all the nodes ending to it.
+   */
+  findANodeWithNoSuccessor() {
+    let hasEdge = false;
+    for (let row = 0; row < this.currentVertices; row++) {
+      hasEdge = false;
+      for (let col = 0; col < this.currentVertices; col++) {
+        if (this.adjacentMatrix[row][col] > 0) {
+          hasEdge = true; //this has the edge start
+          break;
+        }
+      }
+      if (!hasEdge) {
+        return row;
+      }
+    }
+    return -1;
+  }
+  /**
+   * Deleting the vertex from the graph
+   * This will need to update
+   * - vertexList
+   * - adjacency matrix
+   * @param {*} index
+   */
+  deleteVertex(index, debug = false) {
+    debug &&
+      console.log(
+        "deleteVertex condition :",
+        index !== this.currentVertices - 1,
+        "with index ",
+        index,
+        "with currentVertices-1 ",
+        this.currentVertices - 1
+      );
+    //if not deleting the last node
+    if (index !== this.currentVertices - 1) {
+      //removing the vertex from the vertexList
+      //considering 1 less index because we are copying next one to the current one.
+      for (let i = index; i < this.currentVertices - 1; i++) {
+        debug &&
+          console.log(`vertex that will be deleted `, this.vertexList[index]);
+        this.vertexList[i] = this.vertexList[i + 1];
+      }
+      debug &&
+        console.log(
+          this.vertexList
+            .filter((value, index) => index < this.currentVertices)
+            .map((vertex) => vertex.getData())
+            .join(" ")
+        );
+
+      //moving the row up
+      for (let i = index; i < this.currentVertices - 1; i++) {
+        this.moveRowUp(i, this.currentVertices);
+      }
+      debug &&
+        console.log(
+          "Moving the row-up and display the graph:",
+          this.displayGraph()
+        );
+
+      //moving the column left
+      for (let i = index; i < this.currentVertices - 1; i++) {
+        this.moveColumnLeft(i, this.currentVertices - 1);
+      }
+      debug &&
+        console.log(
+          "Moving the column left and display the graph:",
+          this.displayGraph()
+        );
+    }
+    this.currentVertices--;
+  }
+
+  /**
+   * Handling 2 rows at a time and copying 1 in another
+   * @param {*} rowIndex
+   * @param {*} columnCount
+   */
+  moveRowUp(rowIndex, columnCount) {
+    for (let col = 0; col < columnCount; col++) {
+      this.adjacentMatrix[rowIndex][col] = this.adjacentMatrix[rowIndex + 1][
+        col
+      ];
+    }
+  }
+  /**
+   * Moving the columns from right to left
+   * @param {*} colIndex
+   * @param {*} rowCount
+   */
+  moveColumnLeft(colIndex, rowCount) {
+    for (let row = 0; row < rowCount; row++) {
+      this.adjacentMatrix[row][colIndex] = this.adjacentMatrix[row][
+        colIndex + 1
+      ];
+    }
+  }
+
+  /**
+   * Display the adjacency matrix in the graph
+   */
+  displayGraph() {
+    let graph = [];
+    graph.push(
+      "  " +
+        this.vertexList
+          .filter((value, index) => index < this.currentVertices)
+          .map((vertex) => {
+            return vertex.getData();
+          })
+          .join(" ")
+    );
+
+    for (let i = 0; i < this.currentVertices; i++) {
+      graph.push(
+        this.vertexList[i].getData() +
+          " " +
+          this.adjacentMatrix[i]
+            .filter((value, index) => index < this.currentVertices)
+            .join(" ")
+      );
+    }
+    return graph;
+  }
 
   /**
    * Demo the functionality for the Depth first search
@@ -154,7 +291,7 @@ class Graph {
     myGraph.addEdge(0, 3); //AD
     myGraph.addEdge(3, 4); //DE
 
-    console.log(myGraph);
+    console.log(myGraph.displayGraph());
 
     console.log("Doing Depth First search....");
     depthFirstSearch.search(myGraph); //created and exported Object
@@ -168,6 +305,30 @@ class Graph {
     myGraph.unMarkAllVertexAsUnvisited(); //un-marking so that next operation could be performed on the graph
 
     minimumSpanningTree.getMSTFromBreadthFirstSearch(myGraph);
+
+    myGraph = new Graph(8);
+    myGraph.addVertex("A"); //0
+    myGraph.addVertex("B"); //1
+    myGraph.addVertex("C"); //2
+    myGraph.addVertex("D"); //3
+    myGraph.addVertex("E"); //4
+    myGraph.addVertex("F"); //5
+    myGraph.addVertex("G"); //6
+    myGraph.addVertex("H"); //7
+
+    myGraph.addEdge(0, 3, true); //AD
+    myGraph.addEdge(0, 4, true); //AE
+    myGraph.addEdge(1, 4, true); //BE
+    myGraph.addEdge(2, 5, true); //CF
+    myGraph.addEdge(3, 6, true); //DG
+    myGraph.addEdge(4, 6, true); //EG
+    myGraph.addEdge(5, 7, true); //FH
+    myGraph.addEdge(6, 7, true); //GH
+
+    console.log(`Graph as below :-`);
+    console.log(myGraph.displayGraph());
+    console.log("Performing topological sorting");
+    console.log(topologicalSort(myGraph).join(" "));
   }
 }
 
---
+++ src/js/graph/TopologicalSorting.js
@@ -0,0 +1,39 @@
+/**
+ * Topological sorting works for the Directed graph.
+ * This is being used to define the flow of set of tasks
+ * that are to be happened one after the another.
+ * e.g: courses to be taken in a semister.
+ * e.g: capable of showing minimum completion time.
+ */
+module.exports = function sort(graph, debug = false) {
+  let sortedList = new Array(graph.getCurrentVertices());
+  // console.log(sortedList);
+  let vertexList = graph.getVertexList();
+  while (graph.getCurrentVertices() > 0) {
+    let currentVertexIndex = graph.findANodeWithNoSuccessor();
+    debug && console.log("Vertex with No successor :", currentVertexIndex);
+    if (currentVertexIndex === -1) {
+      console.log("Graph has cycles ...");
+      return;
+    } else {
+      sortedList[graph.getCurrentVertices() - 1] = vertexList[
+        currentVertexIndex
+      ].getData();
+      debug && console.log(sortedList);
+      debug && console.log(graph.displayGraph());
+      debug &&
+        console.log(
+          `count of the vertices before deletion : ${graph.getCurrentVertices()}`
+        );
+      graph.deleteVertex(currentVertexIndex);
+      debug &&
+        console.log(
+          `count of the vertices after deletion : ${graph.getCurrentVertices()}`
+        );
+      debug && console.log(graph.displayGraph());
+      debug &&
+        console.log("------------------------------------------------------");
+    }
+  }
+  return sortedList;
+};
